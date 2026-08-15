a219b0c7904d1a5ea0b630a5a66554d06d0f9423
sahad ahmed <sac903299@gmail.com>
2026-08-15 16:35:31 +0600

    undated image module

--- chat/settings.py
+++ chat/settings.py
@@ -136,6 +136,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / "static", 
 ]
